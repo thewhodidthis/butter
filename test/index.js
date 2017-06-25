acac655f7cdffe866b3133f3ebe6eb7bc07ced14
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-26 01:51:09 +0200

    test: check for data in/out

--- test/index.js
+++ test/index.js
@@ -1,8 +1,15 @@
 const test = require('tape');
-const filter = require('../');
+const createFilter = require('../');
 
-test('will return', (t) => {
+test('will operate', (t) => {
+  const source = new ImageData(1, 1);
+
+  const filter = createFilter();
+  const result = filter(source);
+
   t.equal(typeof filter, 'function', 'returns funtion on init');
+  t.ok(result instanceof source.constructor, 'input/output type is a match');
+  t.equal(result.data.length, source.data.length, 'input/output size is a match');
   t.end();
 });
 
