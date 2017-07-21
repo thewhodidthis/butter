e4e5d92463cf3d7e92864fcd3b631dfc5196f5ef
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-21 14:17:27 +0200

    module: use default params, rename cutoff option to threshold

--- index.es
+++ index.es
@@ -1,35 +1,31 @@
+const Butter = ({ threshold = '5e7689', flip = false } = {}) => {
+  // Over or under, black or white
+  const aspect = flip ? -1 : 1
 
-const Butter = (options) => {
-  // Avoid default params for now
-  const config = Object.assign({ cutoff: '5e7689', flip: false }, options)
-
-  // Over or under (black/white)
-  const aspect = config.flip ? -1 : 1
-
-  // Pad threshold color if need be
-  const cutoff = parseInt((`ffffffff${config.cutoff}`).slice(-8), 16)
+  // Pad threshold if need be
+  const cutoff = parseInt((`ffffffff${threshold}`).slice(-8), 16)
 
   // Expects and returns out `ImageData`
-  return (source) => {
-    const { height: h, width: w, data } = source
-    const view = new Uint32Array(data.buffer)
+  return ({ height: h = 1, width: w = 1, data = new Uint8ClampedArray(4) } = {}) => {
+    const result = new ImageData(data, w, h)
+    const view32 = new Uint32Array(data.buffer)
 
-    for (let r = 0; r < h; r += 1) {
-      const start = r * w
+    for (let y = 0; y < h; y += 1) {
+      const start = y * w
       const limit = start + w
-      const range = view.subarray(start, limit)
+      const range = view32.subarray(start, limit)
       const scope = { start: 0, limit: range.length }
 
-      for (let i = 0; i < scope.limit; i += 1) {
-        const color = aspect * range[i]
+      for (let x = 0; x < scope.limit; x += 1) {
+        const color = aspect * range[x]
         const brink = aspect * cutoff
 
         if (color > brink && scope.start === 0) {
-          scope.start = i
+          scope.start = x
         }
 
         if (color < brink && scope.start !== 0) {
-          range.subarray(scope.start, i).sort()
+          range.subarray(scope.start, x).sort()
 
           // Start again
           scope.start = 0
@@ -37,7 +33,7 @@ const Butter = (options) => {
       }
     }
 
-    return source
+    return result
   }
 }
 
--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 'use strict';
 
-var Butter = function (options) {
-  // Avoid default params for now
-  var config = Object.assign({ cutoff: '5e7689', flip: false }, options);
+var Butter = function (ref) {
+  if ( ref === void 0 ) ref = {};
+  var threshold = ref.threshold; if ( threshold === void 0 ) threshold = '5e7689';
+  var flip = ref.flip; if ( flip === void 0 ) flip = false;
 
   // Over or under (black/white)
-  var aspect = config.flip ? -1 : 1;
+  var aspect = flip ? -1 : 1;
 
-  // Pad threshold color if need be
-  var cutoff = parseInt((("ffffffff" + (config.cutoff))).slice(-8), 16);
+  // Pad cutoff if need be
+  var cutoff = parseInt((("ffffffff" + threshold)).slice(-8), 16);
 
   // Expects and returns out `ImageData`
-  return function (source) {
-    var h = source.height;
-    var w = source.width;
-    var data = source.data;
-    var view = new Uint32Array(data.buffer);
+  return function (ref) {
+    if ( ref === void 0 ) ref = {};
+    var h = ref.height; if ( h === void 0 ) h = 1;
+    var w = ref.width; if ( w === void 0 ) w = 1;
+    var data = ref.data; if ( data === void 0 ) data = new Uint8ClampedArray(4);
 
-    for (var r = 0; r < h; r += 1) {
-      var start = r * w;
+    var result = new ImageData(data, w, h);
+    var view32 = new Uint32Array(data.buffer);
+
+    for (var y = 0; y < h; y += 1) {
+      var start = y * w;
       var limit = start + w;
-      var range = view.subarray(start, limit);
+      var range = view32.subarray(start, limit);
       var scope = { start: 0, limit: range.length };
 
-      for (var i = 0; i < scope.limit; i += 1) {
-        var color = aspect * range[i];
+      for (var x = 0; x < scope.limit; x += 1) {
+        var color = aspect * range[x];
         var brink = aspect * cutoff;
 
         if (color > brink && scope.start === 0) {
-          scope.start = i;
+          scope.start = x;
         }
 
         if (color < brink && scope.start !== 0) {
-          range.subarray(scope.start, i).sort();
+          range.subarray(scope.start, x).sort();
 
           // Start again
           scope.start = 0;
@@ -40,7 +44,7 @@ var Butter = function (options) {
       }
     }
 
-    return source
+    return result
   }
 };
 
