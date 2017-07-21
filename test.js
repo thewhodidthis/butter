9b798433cba19e7cf5192b3a57ef6c8cc2cd7a04
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-21 14:18:49 +0200

    test: check for default param handling

--- test.js
+++ test.js
@@ -1,7 +1,17 @@
-require('kpow')()
-
+const kpow = require('kpow')
 const test = require('tape')
 const createFilter = require('./')
+
+kpow()
+
+test('will default', (t) => {
+  const filter = createFilter()
+  const result = filter()
+
+  t.equal(typeof filter, 'function', 'returns funtion on init')
+  t.equal(result.data.length, 4)
+  t.end()
+})
 
 test('will operate', (t) => {
   const source = new ImageData(1, 1)
@@ -9,7 +19,6 @@ test('will operate', (t) => {
   const filter = createFilter()
   const result = filter(source)
 
-  t.equal(typeof filter, 'function', 'returns funtion on init')
   t.ok(result instanceof source.constructor, 'input/output type is a match')
   t.equal(result.data.length, source.data.length, 'input/output size is a match')
   t.end()
