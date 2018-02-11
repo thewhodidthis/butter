c92ad28bf8cf4f06e760b3d3c652fc9ed9a7b712
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-11 11:31:40 +0100

    example: drop iife and .es extension for main script

--- example/index.js
+++ example/index.js
@@ -1,60 +1,54 @@
-(function () {
-'use strict';
-
-var images = document.querySelectorAll('canvas img');
-var boards = document.querySelectorAll('canvas');
+const images = document.querySelectorAll('canvas img')
+const boards = document.querySelectorAll('canvas')
 
 if (window !== window.top) {
-  document.documentElement.classList.add('is-iframe');
+  document.documentElement.classList.add('is-iframe')
 }
 
-var upward = Math.PI * 0.5;
-var params = [
+const upward = Math.PI * 0.5
+const params = [
   { threshold: '332200', up: 1 },
   { threshold: '77838c' },
   { threshold: '81aeb6', up: 1, flip: 0 }
-];
+]
 
-Array.from(images).map(function (img) { return img.alt; }).forEach(function (src, i) {
-  var config = params[i];
-  var canvas = boards[i];
-  var target = canvas.getContext('2d');
+Array.from(images).map(img => img.alt).forEach((src, i) => {
+  const config = params[i]
+  const canvas = boards[i]
+  const target = canvas.getContext('2d')
 
-  var a = config.up ? upward : 0;
-  var y = config.up ? canvas.height : 0;
+  const a = config.up ? upward : 0
+  const y = config.up ? canvas.height : 0
 
-  var buffer = canvas.cloneNode().getContext('2d');
+  const buffer = canvas.cloneNode().getContext('2d')
 
-  buffer.rotate(a);
+  buffer.rotate(a)
 
-  var worker = new Worker('worker.js');
+  const worker = new Worker('worker.js')
 
-  worker.addEventListener('message', function (e) {
-    buffer.putImageData(e.data.result, 0, 0);
+  worker.addEventListener('message', (e) => {
+    buffer.putImageData(e.data.result, 0, 0)
 
-    target.save();
+    target.save()
 
     if (config.up) {
-      target.translate(0, y);
-      target.rotate(-a);
+      target.translate(0, y)
+      target.rotate(-a)
     }
 
-    target.drawImage(buffer.canvas, 0, 0);
-    target.restore();
-  });
+    target.drawImage(buffer.canvas, 0, 0)
+    target.restore()
+  })
 
-  var master = document.createElement('img');
+  const master = document.createElement('img')
 
-  master.addEventListener('load', function () {
-    buffer.drawImage(master, 0, -y);
+  master.addEventListener('load', () => {
+    buffer.drawImage(master, 0, -y)
 
-    var source = buffer.getImageData(0, 0, canvas.height, canvas.width);
+    const source = buffer.getImageData(0, 0, canvas.height, canvas.width)
 
-    worker.postMessage({ config: config, source: source });
-  });
+    worker.postMessage({ config, source })
+  })
 
-  master.setAttribute('src', src);
-});
-
-}());
-
+  master.setAttribute('src', src)
+})
