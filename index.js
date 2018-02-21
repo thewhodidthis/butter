d83b6a9cf45887e3bcd338b2082d3d3c7d2e240f
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-21 08:13:35 +0100

    module: buble not the cjs version

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
 'use strict';
 
-var butter = function (ref) {
-  if ( ref === void 0 ) ref = {};
-  var threshold = ref.threshold; if ( threshold === void 0 ) threshold = '5e7689';
-  var flip = ref.flip; if ( flip === void 0 ) flip = false;
-
+const butter = ({ threshold = '5e7689', flip = false } = {}) => {
   // Over or under, black or white
-  var phase = flip ? -1 : 1;
+  const phase = flip ? -1 : 1;
 
   // Parse threshold, pad if need be
-  var limit = phase * parseInt((("ffffffff" + threshold)).slice(-8), 16);
+  const limit = phase * parseInt((`ffffffff${threshold}`).slice(-8), 16);
 
   // Accepts and returns an `ImageData` like object
-  return function (target) {
-    if ( target === void 0 ) target = { data: [], width: 0, height: 0 };
-
-    var bitmap = new Uint32Array(target.data.buffer);
+  return (target = { data: [], width: 0, height: 0 }) => {
+    const bitmap = new Uint32Array(target.data.buffer);
 
     // Extract dimensions
-    var h = target.height;
-    var w = target.width;
+    const { height: h, width: w } = target;
 
     // Scan through vertically, line by line
-    for (var y = 0; y < h; y += 1) {
+    for (let y = 0; y < h; y += 1) {
       // Chunk start
-      var a = y * w;
+      const a = y * w;
 
       // Chunk stop
-      var b = a + w;
+      const b = a + w;
 
       // Slice up
-      var chunk = bitmap.subarray(a, b);
+      const chunk = bitmap.subarray(a, b);
 
       // Go through each line horizontally, sort past the pixels matching color stop
-      for (var x = 0, start = 0; x < w; x += 1) {
-        var color = phase * chunk[x];
+      for (let x = 0, start = 0; x < w; x += 1) {
+        const color = phase * chunk[x];
 
         // Go ahead
         if (color > limit && start === 0) {
@@ -56,4 +49,3 @@ var butter = function (ref) {
 };
 
 module.exports = butter;
-
