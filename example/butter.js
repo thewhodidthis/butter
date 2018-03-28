cd80553ba52c47d288b596ed9b9b95503eab3525
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-03-28 15:41:36 +0200

    package/example: go umd with web worker import

--- example/butter.js
+++ example/butter.js
@@ -1,54 +1,57 @@
-var butter = (function () {
-'use strict';
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.butter = factory());
+}(this, (function () { 'use strict';
 
-const butter = ({ threshold = '5e7689', flip = false } = {}) => {
-  // Over or under, black or white
-  const phase = flip ? -1 : 1;
+  const butter = ({ threshold = '5e7689', flip = false } = {}) => {
+    // Over or under, black or white
+    const phase = flip ? -1 : 1;
 
-  // Parse threshold, pad if need be
-  const limit = phase * parseInt((`ffffffff${threshold}`).slice(-8), 16);
+    // Parse threshold, pad if need be
+    const limit = phase * parseInt((`ffffffff${threshold}`).slice(-8), 16);
 
-  // Accepts and returns an `ImageData` like object
-  return (target = { data: [], width: 0, height: 0 }) => {
-    const bitmap = new Uint32Array(target.data.buffer);
+    // Accepts and returns an `ImageData` like object
+    return (target = { data: [], width: 0, height: 0 }) => {
+      const bitmap = new Uint32Array(target.data.buffer);
 
-    // Extract dimensions
-    const { height: h, width: w } = target;
+      // Extract dimensions
+      const { height: h, width: w } = target;
 
-    // Scan through vertically, line by line
-    for (let y = 0; y < h; y += 1) {
-      // Chunk start
-      const a = y * w;
+      // Scan through vertically, line by line
+      for (let y = 0; y < h; y += 1) {
+        // Chunk start
+        const a = y * w;
 
-      // Chunk stop
-      const b = a + w;
+        // Chunk stop
+        const b = a + w;
 
-      // Slice up
-      const chunk = bitmap.subarray(a, b);
+        // Slice up
+        const chunk = bitmap.subarray(a, b);
 
-      // Go through each line horizontally, sort past the pixels matching color stop
-      for (let x = 0, start = 0; x < w; x += 1) {
-        const color = phase * chunk[x];
+        // Go through each line horizontally, sort past the pixels matching color stop
+        for (let x = 0, start = 0; x < w; x += 1) {
+          const color = phase * chunk[x];
 
-        // Go ahead
-        if (color > limit && start === 0) {
-          start = x;
-        }
+          // Go ahead
+          if (color > limit && start === 0) {
+            start = x;
+          }
 
-        // Time's up
-        if (color < limit && start !== 0) {
-          chunk.subarray(start, x).sort();
+          // Time's up
+          if (color < limit && start !== 0) {
+            chunk.subarray(start, x).sort();
 
-          // Start again
-          start = 0;
+            // Start again
+            start = 0;
+          }
         }
       }
+
+      return target
     }
+  };
 
-    return target
-  }
-};
+  return butter;
 
-return butter;
-
-}());
+})));
